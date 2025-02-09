5edd184af4dc43e62b041b06a3f13233b445184a
Fábio Pereira <fabiopereirasilva.dev@gmail.com>
2025-02-08 22:47:29 -0300

    Adicionar arquivos de projeto.

---
+++ TestandoAPI/Models/TarefaModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TestandoAPI.Models
+{
+    public class TarefaModel
+    {
+
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public int Status { get; set; }
+
+    }
+}
