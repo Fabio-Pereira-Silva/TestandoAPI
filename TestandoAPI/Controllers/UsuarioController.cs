669329047c74f639ec171af23e53fa76bb682688
Fábio Pereira <fabiopereirasilva.dev@gmail.com>
2025-02-08 22:55:11 -0300

    Testando swagger

---
+++ TestandoAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestandoAPI.Models;
+
+namespace TestandoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+
+        [HttpGet]
+
+        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+
+            return Ok();
+        }
+    }
+}
